4b3daea3764f9d1aff39db4b27545b049d2f3273
mjoviedo <mjoviedo@isep-cba.edu.ar>
2024-05-03 16:55:36 -0300

    comentarios en js y css

--- scripts.js
+++ scripts.js
@@ -1,11 +1,8 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* setea el ancho del menu del indice al abrir y cerrar */
 function openNav() {
     document.getElementById("mySidenav").style.width = "400px";
-    /*document.getElementById("main").style.marginLeft = "400px";*/
   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    /*document.getElementById("main").style.marginLeft = "0";*/
   }
